849e9127dc6f355f68d139849b0fc60256a54b69
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2021-04-01 15:10:31 +0200

    add index and style

---
+++ index.js
@@ -0,0 +1,58 @@
+import "./style.css";
+
+const dom = document.createElement("div");
+
+function Message() {
+  dom.classList.add("react-notification");
+  window.onload = () => document.getElementById("root").appendChild(dom);
+}
+
+Message.prototype.show = function (
+  content,
+  duration = 3000,
+  type = "info",
+  onClose = Function.prototype
+) {
+  const contentBox = document.createElement("div");
+  const contentDom = document.createElement("span");
+  const icon = document.createElement("i");
+  icon.classList.add(type);
+  icon.classList.add("react-notification-icon");
+  contentDom.innerText = content;
+  contentBox.classList.add("react-content-box");
+  contentBox.classList.add("animate-in");
+  contentBox.appendChild(icon);
+  contentBox.appendChild(contentDom);
+  contentBox.style.top = `${this.count * 50}px`;
+  dom.appendChild(contentBox);
+
+  this.count++;
+
+  // remove message box after duration
+  setTimeout(() => {
+    contentBox.classList.add("animate-out");
+    setTimeout(() => {
+      dom.removeChild(contentBox);
+
+      const boxs = document.querySelectorAll(".react-content-box");
+      for (let i = 0; i < boxs.length; i++) {
+        boxs[i].style.top = `${parseInt(boxs[i].style.top, 10) - 50}px`;
+      }
+      this.count--;
+
+      if (typeof onClose === "function") onClose();
+    }, 300);
+  }, duration);
+};
+
+// API
+["success", "error", "warn", "info"].forEach((method) => {
+  Message.prototype[method] = function (content, duration, onClose) {
+    this.show(content, duration, method, onClose);
+  };
+});
+
+// the count of messages already exist
+Message.prototype.count = 0;
+
+export default new Message();
